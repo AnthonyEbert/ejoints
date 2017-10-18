34bd176b27bf5b156af9ca20e5cbb8c62b8465cd
Anthony <anthonyebert@gmail.com>
2017-10-18 17:06:17 +1000

    Initial commit.

---
+++ R/ejoints.R
@@ -0,0 +1,80 @@
+
+
+
+create_ejoint <- function(input, sigma){
+  output <- list(input = input, sigma = sigma)
+  return(output)
+}
+
+
+mvrnorm_ejoint <- function(mu, sigma){
+  mu <- as.vector(mu)
+
+  output <- MASS::mvrnorm(
+    n = 1,
+    mu = mu,
+    Sigma = sigma * diag(length(mu))
+  )
+  return(output)
+}
+
+dmvnorm_ejoint <- function(mu, sigmai, x, threshold = 50){
+  x <- as.vector(x)
+  mu <- as.vector(mu)
+
+  indic <- stats::dist(mu - x)/sigmai < threshold
+
+  if(indic){
+    output <- mvtnorm::dmvnorm(x, mean = mu, sigma = sigmai * diag(length(mu)))
+  } else {
+    output <- 0
+  }
+
+  return(output)
+}
+
+
+#' Sample from empirical joint density
+#' @param n number of samples required
+#' @param input data in matrix form
+#' @param sigma the standard deviation used to construct the empirical density
+#' @export
+rejoint <- function(n, input, sigma = 1){
+
+  input <- as.matrix(input)
+  m <- dim(input)[1]
+
+  sample_i <- sample.int(m, n, replace = TRUE)
+
+  output <- apply(input[sample_i,], 1, mvrnorm_ejoint, sigma = sigma)
+
+  return(output)
+}
+
+
+#' Sample from empirical joint density
+#' @param x the point to be evaluated
+#' @param input data in matrix form
+#' @param sigma the standard deviation used to construct the empirical
+#' @export
+dejoint <- function(x, input, sigma = 1){
+
+  x <- as.vector(x)
+  input <- as.matrix(input)
+
+  stopifnot(length(x) == dim(input)[2])
+
+  output <- apply(input, 1, dmvnorm_ejoint, x = x, sigmai = sigma)
+
+  return(sum(output)/dim(input)[1])
+}
+
+dejoint2 <- function(x, y, input, sigma = 1){
+
+  input <- as.matrix(input)
+
+
+  output <- apply(input, 1, dmvnorm_ejoint, x = c(x,y), sigmai = sigma)
+
+  return(sum(output)/dim(input)[1])
+}
